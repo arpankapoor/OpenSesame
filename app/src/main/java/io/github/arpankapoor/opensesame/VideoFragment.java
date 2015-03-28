741e429093fbc33e83e70aa543c65c6fb912e504
Arpan Kapoor <k.arpan26@gmail.com>
2015-03-28 12:38:07 +0530

    Add progressbar to the VideoFragment

--- app/src/main/java/io/github/arpankapoor/opensesame/VideoFragment.java
+++ app/src/main/java/io/github/arpankapoor/opensesame/VideoFragment.java
@@ -1,17 +1,17 @@
 package io.github.arpankapoor.opensesame;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
 
 
 public class VideoFragment extends Fragment {
-
-    private VideoView mVideoView;
 
     public VideoFragment() {
         // Required empty public constructor
@@ -34,8 +34,28 @@ public class VideoFragment extends Fragment {
         String vidAddr = "http://192.168.0.100:8080/arp.webm";
         Uri vidUri = Uri.parse(vidAddr);
 
-        mVideoView = (VideoView) getActivity().findViewById(R.id.videoview);
+        final ProgressBar progressBar = (ProgressBar) getActivity().findViewById(R.id.progressbar);
+        VideoView mVideoView = (VideoView) getActivity().findViewById(R.id.videoview);
         mVideoView.setVideoURI(vidUri);
         mVideoView.start();
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        mVideoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int i, int i2) {
+                mp.stop();
+                progressBar.setVisibility(View.INVISIBLE);
+                return true;
+            }
+        });
     }
 }
